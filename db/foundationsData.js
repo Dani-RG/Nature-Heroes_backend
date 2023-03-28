bedf7b0209c25376332b24828f328e2a2565e69a
Daniel-RG <21danielrondong@gmail.com>
2023-03-28 21:00:18 +0200

    remove animals array in foundations

--- db/foundationsData.js
+++ db/foundationsData.js
@@ -5,6 +5,5 @@ module.exports = [
     acronym: 'IUCN',
     description: 'It is a membership Union uniquely composed of both government and civil society organisations. By harnessing the experience, resources and reach of its more than 1,400 Member organisations and the input of some 15,000 experts, IUCN is the global authority on the status of the natural world and the measures needed to safeguard it.',
     logo: 'https://nc.iucnredlist.org/redlist/partners/images/1521112071-iucn.png',
-    animal: []
   },
 ]
